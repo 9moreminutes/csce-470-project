86f5489f1fba49368d6ac4e47ed449eff00a8316
Matthew Horton <mtt.horton@gmail.com>
2014-12-02 18:45:25 -0600

    adding plan for search function in comments

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,10 @@ def hello():
 
 @app.route("/trackflix/search")
 def search():
+	#connect to mongo
+	# get the top 10 movies for the searhc
+	# get the image url for each movie
+	# {("Title", "image url"): [("Title", "image url"), ("Title", "image url"), ("Title", "image url"), ("Title", "image url")]}
 	return "This is the search result"
 
 if __name__ == "__main__":
